1141e3ba28af5808f4a667b72961aea1104f3232
eyubkh <eyub.kh@gmail.com>
2023-01-05 11:28:18 +0000

    adding new component sub header

---
+++ components/molecules/WindowSubHeader.tsx
@@ -0,0 +1,27 @@
+import { Black100, White100 } from '@utils/tokens'
+import styled from 'styled-components'
+
+const WindowSubHeaderComponent = styled.div`
+  display: flex;
+  width: 100%;
+  background: ${White100};
+  padding: 5px 5px;
+  border: 1px solid ${Black100};
+  border-top: none;
+  gap: 10px;
+  h3:first-letter {
+    text-decoration: underline;
+    text-transform: uppercase;
+  }
+`
+
+export const WindowSubHeader = () => {
+  return(
+    <WindowSubHeaderComponent>
+      <h3>File</h3>
+      <h3>Options</h3>
+      <h3>Window</h3>
+      <h3>Help</h3>
+    </WindowSubHeaderComponent>
+  )
+}
