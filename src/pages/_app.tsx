86c1bdaa11d9dc15414857256eb1e966750bac60
eyubkh <eyub.kh@gmail.com>
2023-01-05 11:24:50 +0000

    adding fonts files and font style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
+
+const myFont = localFont({ src: [
+  {
+    path: '../../public/fonts/sysfont.woff2',
+    weight: 'bold'
+  },
+  {
+    path: '../../public/fonts/w95fa.woff2',
+    weight: 'normal'
+  },
+]})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
