3eb359dbdd8e6155cc94a5adaa9394299984d654
eyubkh <eyub.kh@gmail.com>
2023-01-08 16:31:25 +0000

    adding window provider

--- components/organisms/ProgramManager.tsx
+++ components/organisms/ProgramManager.tsx
@@ -1,35 +1,23 @@
 import styled from 'styled-components'
-import Draggable from 'react-draggable'
-import { useRef } from 'react'
-import { WindowHeader } from 'components/molecules/WindowHeader'
-import { WindowSubHeader } from 'components/molecules/WindowSubHeader'
 import { White100 } from '@utils/tokens'
+import { Window } from './Window'
+import { IconAboutMe } from 'components/molecules/IconAboutMe'
+import { WindowProvider } from 'context/windowContext'
+
 
 const ProgramManagerComponent = styled.div`
   place-content: center;
-  width: 400px;
-  height:  400px;
+  width: 500px;
+  height:  300px;
   background-color: ${White100};
 `
 
 export const ProgramManager = () => {
-
-  const nodeRef = useRef(null)
-
-  const handlerDrag = (event: any) => {
-    // console.log(event, 'draggggging')
-  } 
-
   return (
-        <Draggable
-        handle='strong'
-        // nodeRef={nodeRef}
-          onDrag={handlerDrag}
-        > 
-          <ProgramManagerComponent>
-            <WindowHeader />
-            <WindowSubHeader />
-          </ProgramManagerComponent>
-        </Draggable> 
+    <WindowProvider>
+      <Window title='Program Manager'>
+        <IconAboutMe />
+      </Window>
+    </WindowProvider>
   )
 }
