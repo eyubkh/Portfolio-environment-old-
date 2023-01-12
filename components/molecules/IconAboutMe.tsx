117715f1d9305280ecfdc8527cb2d296b9c12ba4
eyubkh <eyub.kh@gmail.com>
2023-01-12 14:57:07 +0000

    using icon component to create aboutme and projects icons

--- components/molecules/IconAboutMe.tsx
+++ components/molecules/IconAboutMe.tsx
@@ -1,30 +1,5 @@
-import { Icon } from "components/atoms/Icon"
-import Draggable from "react-draggable"
-import styled from "styled-components"
-
-const IconAboutMeComponent = styled.div`
-  display: inline-block;
-  text-align: center;
-  background-color: #f5f5f5;
-  padding: 12px;
-`
+import { Icon } from "./Icon"
 
 export const IconAboutMe = (): JSX.Element => {
-
-  const handlerClick = () => {
-    console.log('hello')
-  }
-
-  return(
-    <Draggable>
-      <IconAboutMeComponent
-        onClick={handlerClick}
-      >
-        <Icon
-          src="/icons/PROGM024.ICO"
-        />
-          <p>About Me</p>
-      </IconAboutMeComponent>
-    </Draggable>
-  )
+  return <Icon src="/icons/PROGM024.ICO">About Me</Icon>
 }
---
+++ components/molecules/IconProjects.tsx
@@ -0,0 +1,5 @@
+import { Icon } from "./Icon"
+
+export const IconProjects = (): JSX.Element => {
+  return <Icon src="/icons/PROGM008.ICO">Projects</Icon>
+}
