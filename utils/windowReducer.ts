7e1cb2c6b9436007c9db8c923c12f82988e5bb41
eyubkh <eyub.kh@gmail.com>
2023-01-12 14:32:27 +0000

    moving window reducer to utlis folder

---
+++ utils/windowReducer.ts
@@ -0,0 +1,25 @@
+import { Reducer } from 'react'
+import { WindowStateProps, ActionProps, ActionOptions } from 'types/context/windowTypes'
+
+const windowReducer: Reducer<WindowStateProps, ActionProps> = (state, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case ActionOptions.FULLSCREEN: {
+      return {
+        ...state,
+        isFullScreen: !state.isFullScreen
+      }
+    }
+    case ActionOptions.OPEN: {
+      return {
+        ...state,
+        isOpen: !state.isOpen
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+export default windowReducer
