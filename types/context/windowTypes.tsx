8da2d202331b1fa3e02df91c2da55d5f8c036f62
eyubkh <eyub.kh@gmail.com>
2023-01-12 14:32:43 +0000

    adding types for window context

---
+++ types/context/windowTypes.tsx
@@ -0,0 +1,13 @@
+export enum ActionOptions {
+  FULLSCREEN,
+  OPEN,
+  LIMITED
+}
+
+export type WindowStateProps = {
+  isFullScreen: boolean,
+  isOpen: boolean,
+  limited: boolean,
+}
+
+export type ActionProps = { type: ActionOptions, payload?: boolean }
