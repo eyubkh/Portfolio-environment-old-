556511cbc76a9a35cae09b6dd9558c32b686b77f
eyubkh <eyub.kh@gmail.com>
2023-01-13 11:02:17 +0000

    removing unused elements

--- components/organisms/ProgramManager.tsx
+++ components/organisms/ProgramManager.tsx
@@ -1,22 +1,14 @@
-import styled from 'styled-components'
-import { White100 } from '@utils/tokens'
 import { Window } from './Window'
 import { IconAboutMe } from 'components/molecules/IconAboutMe'
-import { WindowProvider } from 'context/windowContext'
-
-
-const ProgramManagerComponent = styled.div`
-  place-content: center;
-  width: 500px;
-  height:  300px;
-  background-color: ${White100};
-`
+import { WindowProvider } from 'lib/windowContext'
+import { IconProjects } from 'components/molecules/IconProjects'
 
 export const ProgramManager = () => {
   return (
     <WindowProvider>
       <Window title='Program Manager'>
         <IconAboutMe />
+        <IconProjects />
       </Window>
     </WindowProvider>
   )
