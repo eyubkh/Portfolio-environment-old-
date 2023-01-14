e5c0b24ac26dcd81d8c40630babbd896c5cc4643
eyubkh <eyub.kh@gmail.com>
2023-01-14 12:15:54 +0000

    adding styled component

--- components/pages/Os.tsx
+++ components/pages/Os.tsx
@@ -1,9 +1,14 @@
-import { ProgramManager } from "../organisms/ProgramManager";
+import styled from "styled-components"
+import { ProgramManager } from "../organisms/ProgramManager"
+
+const OsComponent = styled.div<any>`
+  height: 100vh;
+`
 
 export function Os() {
   return (
-    <>
+    <OsComponent>
       <ProgramManager />
-    </>
+    </OsComponent>
   )
 }
