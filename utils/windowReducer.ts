fc78973952f43847f62b123eaae922ce3de6fd45
eyubkh <eyub.kh@gmail.com>
2023-01-14 12:14:19 +0000

    adding new Actions and renaming window action type

--- utils/windowReducer.ts
+++ utils/windowReducer.ts
@@ -1,19 +1,34 @@
 import { Reducer } from 'react'
-import { WindowStateProps, ActionProps, ActionOptions } from 'types/lib/windowTypes'
+import { WindowStateProps, WindowActionProps, WindowActionOptions } from 'types/lib/windowTypes'
 
-const windowReducer: Reducer<WindowStateProps, ActionProps> = (state, action) => {
+const windowReducer: Reducer<WindowStateProps, WindowActionProps> = (state, action) => {
   const { type, payload } = action
+  const { FULLSCREEN, OPEN, ID, POSSITION } = WindowActionOptions
   switch (type) {
-    case ActionOptions.FULLSCREEN: {
+    case FULLSCREEN: {
       return {
         ...state,
-        isFullScreen: !state.isFullScreen
+        isFullScreen: !state.isFullScreen,
+        possition: !state.isFullScreen ? { x: 0, y: 0 } : state.lastPossition
       }
     }
-    case ActionOptions.OPEN: {
+    case OPEN: {
       return {
         ...state,
         isOpen: !state.isOpen
+      }
+    }
+    case ID: {
+      return {
+        ...state,
+        id: '123456' + Math.round(Math.random() * 10)
+      }
+    }
+    case POSSITION: {
+      return {
+        ...state,
+        lastPossition: payload,
+        possition: payload
       }
     }
     default: {
