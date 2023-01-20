0470ca60234ca79381a64cae0054c5cc7ce564b8
eyubkh <eyub.kh@gmail.com>
2023-01-20 11:29:42 +0100

    adding new process case

--- utils/processReducer.ts
+++ utils/processReducer.ts
@@ -1,9 +1,16 @@
 import { Reducer } from "react"
-import { ProcessActionProps, ProcessStateTypes } from "types/lib/processTypes"
+import { ProcessActionOptions, ProcessActionProps, ProcessStateTypes } from "types/lib/processTypes"
 
 export const processReducer: Reducer<ProcessStateTypes, ProcessActionProps> = (state, action) => {
   const { type, payload } = action
+  const { PROCESS } = ProcessActionOptions
   switch (type) {
+    case PROCESS: {
+      return {
+        ...state,
+        processes: [...state.processes, payload]
+      }
+    }
     default: {
       return state
     }
