adf036b675391130717f6ea318c0b7d915a12901
eyubkh <eyub.kh@gmail.com>
2023-01-20 11:29:03 +0100

    adding process provider

--- src/pages/os.tsx
+++ src/pages/os.tsx
@@ -1,5 +1,10 @@
+import { ProcessProvider } from "lib/processContext";
 import { Os } from "../../components/pages/Os";
 
-export default function OsPage () {
-  return <Os />
+export default function OsPage() {
+  return (
+    <ProcessProvider>
+      <Os />
+    </ProcessProvider>
+  )
 }
