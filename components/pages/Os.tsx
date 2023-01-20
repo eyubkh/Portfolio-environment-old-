cb936be9ce128e336dfaf2200766d6f96c841dfc
eyubkh <eyub.kh@gmail.com>
2023-01-20 11:28:27 +0100

    using process context and redering processes

--- components/pages/Os.tsx
+++ components/pages/Os.tsx
@@ -1,17 +1,24 @@
 import styled from "styled-components"
 import { ProgramManager } from "../organisms/ProgramManager"
-import { ProcessProvider } from "lib/processContext"
+import { ProcessContext, ProcessProvider } from "lib/processContext"
+import { useContext } from "react"
+import { IconProcesses } from "components/molecules/IconProcesses"
 
 const OsComponent = styled.div<any>`
   height: 100vh;
 `
 
 export function Os() {
+  const { state } = useContext(ProcessContext)
+  console.log(state)
   return (
-    <ProcessProvider>
-      <OsComponent>
-        <ProgramManager />
-      </OsComponent>
-    </ProcessProvider>
+    <OsComponent>
+      <ProgramManager />
+      {
+        state?.processes.map(() => {
+          return <IconProcesses />
+        })
+      }
+    </OsComponent>
   )
 }
