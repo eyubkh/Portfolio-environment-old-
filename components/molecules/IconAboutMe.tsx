407e9cc740cd33a4b32f0fe5dfb14a3fa3c0ed5e
eyubkh <eyub.kh@gmail.com>
2023-01-24 09:14:26 +0100

    adding dipatch process and handler on click

--- components/molecules/IconAboutMe.tsx
+++ components/molecules/IconAboutMe.tsx
@@ -1,5 +1,24 @@
+import { useContext } from "react"
 import { Icon } from "./Icon"
+import { ProcessContext } from "lib/processContext"
+import { ProcessActionOptions } from "types/lib/processTypes"
+import { AboutMe } from "components/organisms/AboutMe"
 
 export const IconAboutMe = (): JSX.Element => {
-  return <Icon src="/icons/PROGM024.ICO">About Me</Icon>
+  const { dispatch: addProcesses } = useContext(ProcessContext)
+  const handlerClick = (event: any) => {
+    event.preventDefault()
+    addProcesses({
+      type: ProcessActionOptions.PROCESSES,
+      payload: <AboutMe />
+    })
+  }
+  return (
+    <Icon
+      handler={handlerClick}
+      src="/icons/PROGM024.ICO"
+    >
+      About Me
+    </Icon>
+  )
 }
