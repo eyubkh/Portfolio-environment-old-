9707b36a582de3c555155887d54399cd1315ed99
eyubkh <eyub.kh@gmail.com>
2023-01-25 14:58:25 +0100

    adding new cases

--- utils/processReducer.ts
+++ utils/processReducer.ts
@@ -3,12 +3,18 @@ import { ProcessActionOptions, ProcessActionProps, ProcessStateTypes } from "typ
 
 export const processReducer: Reducer<ProcessStateTypes, ProcessActionProps> = (state, action) => {
   const { type, payload } = action
-  const { PROCESSES } = ProcessActionOptions
+  const { PROCESSES, ICONPROCESSES } = ProcessActionOptions
   switch (type) {
     case PROCESSES: {
       return {
         ...state,
         processes: [...state.processes, payload]
+      }
+    }
+    case ICONPROCESSES: {
+      return {
+        ...state,
+        iconProcesses: [...state.iconProcesses, payload]
       }
     }
     default: {
--- utils/windowReducer.ts
+++ utils/windowReducer.ts
@@ -3,7 +3,7 @@ import { WindowStateProps, WindowActionProps, WindowActionOptions } from 'types/
 
 const windowReducer: Reducer<WindowStateProps, WindowActionProps> = (state, action) => {
   const { type, payload } = action
-  const { FULLSCREEN, OPEN, ID, POSSITION } = WindowActionOptions
+  const { FULLSCREEN, ICON, TITLE, ID, POSSITION } = WindowActionOptions
   switch (type) {
     case FULLSCREEN: {
       return {
@@ -12,16 +12,22 @@ const windowReducer: Reducer<WindowStateProps, WindowActionProps> = (state, acti
         possition: !state.isFullScreen ? { x: 0, y: 0 } : state.lastPossition
       }
     }
-    case OPEN: {
-      return {
-        ...state,
-        isOpen: !state.isOpen
-      }
-    }
     case ID: {
       return {
         ...state,
         id: '123456' + Math.round(Math.random() * 10)
+      }
+    }
+    case TITLE: {
+      return {
+        ...state,
+        title: payload
+      }
+    }
+    case ICON: {
+      return {
+        ...state,
+        icon: payload
       }
     }
     case POSSITION: {
