eb64079456e8426f8d0fcf1c5d3deafb991cdaab
eyubkh <eyub.kh@gmail.com>
2023-01-27 18:51:02 +0100

    dispatching process to deleting icon

--- components/molecules/IconProcesses.tsx
+++ components/molecules/IconProcesses.tsx
@@ -1,11 +1,23 @@
+import { IconProcessesTypes, ProcessActionOptions } from "types/lib/processTypes"
 import { Icon } from "./Icon"
+import { useContext } from "react"
+import { ProcessContext } from "lib/processContext"
 
-export const IconProcesses = ({ id, title, icon }: { id: string, title: string, icon: string }): JSX.Element => {
+export const IconProcesses = ({ id, title, icon, processPossition }: IconProcessesTypes): JSX.Element => {
+  const { dispatch: dispatchProcess } = useContext(ProcessContext)
   const handlerOnClick = (event: any) => {
+    console.log(processPossition)
     event.preventDefault()
     const el = document.getElementById(id)
     if (el) {
       el.style.display = 'inherit'
+    }
+
+    if (dispatchProcess) {
+      dispatchProcess({
+        type: ProcessActionOptions.DELETE_ICON_PROCESS,
+        payload: processPossition
+      })
     }
 
   }
