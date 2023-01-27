cf02b2d7668381606cc640c34192a8f423d12833
eyubkh <eyub.kh@gmail.com>
2023-01-27 18:52:13 +0100

    removing unused code

--- components/organisms/ProgramManager.tsx
+++ components/organisms/ProgramManager.tsx
@@ -2,6 +2,7 @@ import { Window } from './Window'
 import { IconAboutMe } from 'components/molecules/IconAboutMe'
 import { WindowProvider } from 'lib/windowContext'
 import { IconProjects } from 'components/molecules/IconProjects'
+import { useEffect } from 'react'
 
 export const ProgramManager = () => {
   return (
