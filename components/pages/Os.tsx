19c1ed446f61b333094c02cd37c83a3adfb7c370
eyubkh <eyub.kh@gmail.com>
2023-01-27 18:53:21 +0100

    maping porcess array and filtering and addinf logic

--- components/pages/Os.tsx
+++ components/pages/Os.tsx
@@ -3,6 +3,7 @@ import { ProgramManager } from "../organisms/ProgramManager"
 import { ProcessContext, ProcessProvider } from "lib/processContext"
 import { useContext } from "react"
 import { IconProcesses } from "components/molecules/IconProcesses"
+import { IconProcessesTypes } from "types/lib/processTypes"
 
 const OsComponent = styled.div<any>`
   height: 100vh;
@@ -15,13 +16,22 @@ export function Os() {
       <div id="processes">
         {
           state?.processes
+            .filter((item: any) => {
+              if (item) return item
+            })
         }
       </div>
       <div id="icons">
         {
-          state?.iconProcesses.map(({ id, title, icon }: { id: string, title: string, icon: string }, index: number) => {
-            return <IconProcesses key={index} id={id} title={title} icon={icon} />
-          })
+          state?.iconProcesses
+            .map((object: IconProcessesTypes | undefined, index: number) => {
+              if (!object) return undefined
+              const { id, title, icon } = object
+              return <IconProcesses key={id + index} id={id} title={title} processPossition={index} icon={icon} />
+            })
+            .filter((item: any) => {
+              if (item) return item
+            })
         }
       </div>
     </OsComponent>
