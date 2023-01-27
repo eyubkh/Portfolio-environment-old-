45c711a2e92d62d31deecf24485b7082624485a7
eyubkh <eyub.kh@gmail.com>
2023-01-27 18:54:40 +0100

    adding delete processes and delete icons process

--- utils/processReducer.ts
+++ utils/processReducer.ts
@@ -3,7 +3,14 @@ import { ProcessActionOptions, ProcessActionProps, ProcessStateTypes } from "typ
 
 export const processReducer: Reducer<ProcessStateTypes, ProcessActionProps> = (state, action) => {
   const { type, payload } = action
-  const { PROCESSES, ICONPROCESSES } = ProcessActionOptions
+
+  const {
+    PROCESSES,
+    ICON_PROCESSES,
+    DELETE_ICON_PROCESS,
+    DELETE_PROCESSES
+  } = ProcessActionOptions
+
   switch (type) {
     case PROCESSES: {
       return {
@@ -11,10 +18,24 @@ export const processReducer: Reducer<ProcessStateTypes, ProcessActionProps> = (s
         processes: [...state.processes, payload]
       }
     }
-    case ICONPROCESSES: {
+    case DELETE_PROCESSES: {
+      state.processes[payload] = undefined
+      return {
+        ...state,
+        processes: [...state.processes]
+      }
+    }
+    case ICON_PROCESSES: {
       return {
         ...state,
         iconProcesses: [...state.iconProcesses, payload]
+      }
+    }
+    case DELETE_ICON_PROCESS: {
+      state.iconProcesses[payload] = undefined
+      return {
+        ...state,
+        iconProcesses: [...state.iconProcesses]
       }
     }
     default: {
