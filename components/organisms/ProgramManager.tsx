1194e96bcd6f5cbc691dac0cc45a4e6c44188316
eyubkh <eyub.kh@gmail.com>
2023-01-28 18:38:23 +0100

    formatting

--- components/organisms/ProgramManager.tsx
+++ components/organisms/ProgramManager.tsx
@@ -2,7 +2,6 @@ import { Window } from './Window'
 import { IconAboutMe } from 'components/molecules/IconAboutMe'
 import { WindowProvider } from 'lib/windowContext'
 import { IconProjects } from 'components/molecules/IconProjects'
-import { useEffect } from 'react'
 
 export const ProgramManager = () => {
   return (
