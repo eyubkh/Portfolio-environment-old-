cf2b9461472419bb41af8ad109d2ecdf65629f76
eyubkh <eyub.kh@gmail.com>
2023-01-31 16:44:52 +0100

    fetchnig api and parsering readme text to html

--- components/organisms/AboutMe.tsx
+++ components/organisms/AboutMe.tsx
@@ -1,27 +1,27 @@
 import { WindowProvider } from "lib/windowContext"
-import Image from "next/image"
 import { Window } from "./Window"
 import { aboutMe } from "@utils/data"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import parse from 'html-react-parser'
+
 export const AboutMe = (): JSX.Element => {
+  const [readme, setReadme] = useState('')
   useEffect(() => {
-    (async function fechingMD() {
+    (async function () {
       const response = await window.fetch('/api/fetchmd')
+        .then(data => data.text())
       console.log(response)
+      setReadme(response)
     })()
   }, [])
+
   return (
     <WindowProvider>
       <Window
         title={aboutMe.title}
         icon={aboutMe.icon}
       >
-        <Image
-          src="/img/me.png"
-          alt="Photo of Ayoub Khalfaoui Hadi"
-          height={200}
-          width={200}
-        />
+        <div>{parse(readme)}</div>
       </Window>
     </WindowProvider>
   )
