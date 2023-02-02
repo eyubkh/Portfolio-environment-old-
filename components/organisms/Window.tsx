801b940187d9685c4285fadc39fd51fcf12ccc18
eyubkh <eyub.kh@gmail.com>
2023-02-02 16:05:33 +0100

    passing new prop to component and removing icon dispatch

--- components/organisms/Window.tsx
+++ components/organisms/Window.tsx
@@ -8,6 +8,7 @@ import { WindowActionOptions } from "types/lib/windowTypes"
 import { WindowTypes } from "types/global"
 import useWindowContext from "@utils/useWindowContext"
 import handlerOnControlledDrag from "@utils/handlerOnControlledDrag"
+import useProcessContext from "@utils/useProcessContext"
 
 const WindowComponent = styled.div<any>`
   position: absolute;
@@ -16,19 +17,20 @@ const WindowComponent = styled.div<any>`
   border: 1px solid red;
   width: ${({ isFullScreen, width }) => isFullScreen ? '100%' : width + 'px'};
   height: ${({ isFullScreen, height }) => isFullScreen ? '100vh' : height + 'px'};
+  display: ${({ minimized }) => minimized ? 'none' : 'inherit'};
 `
 
 export const Window = ({ title, icon, children }: WindowTypes): JSX.Element => {
   const { state: windowState, dispatch: windowDispatch } = useWindowContext()
   const { isFullScreen, height, width, possition } = windowState
 
+  const { state: processState } = useProcessContext()
+  const { minimized, id } = processState.processes[title]
+  console.log(minimized)
   useEffect(() => {
     windowDispatch({
-      type: WindowActionOptions.ID
-    })
-    windowDispatch({
-      type: WindowActionOptions.ICON,
-      payload: icon
+      type: WindowActionOptions.ID,
+      id
     })
     windowDispatch({
       type: WindowActionOptions.TITLE,
@@ -39,7 +41,8 @@ export const Window = ({ title, icon, children }: WindowTypes): JSX.Element => {
   const props = {
     height,
     width,
-    isFullScreen
+    isFullScreen,
+    minimized
   }
 
   return (
@@ -50,7 +53,7 @@ export const Window = ({ title, icon, children }: WindowTypes): JSX.Element => {
       onDrag={(event: any, position: any) => handlerOnControlledDrag(event, position, windowDispatch)}
     >
       <WindowComponent id={windowState.id} {...props} >
-        <WindowHeader title={title} />
+        <WindowHeader />
         <WindowSubHeader />
         <WindowContent>
           {children}
