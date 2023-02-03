0d957f02c8c97910fe428161340658384d0fe383
eyubkh <eyub.kh@gmail.com>
2023-02-03 19:02:35 +0100

    adding projects

--- utils/data.tsx
+++ utils/data.tsx
@@ -1,17 +1,24 @@
 import { AboutMe } from "components/organisms/AboutMe"
+import { Englicus } from "components/organisms/Englicus"
+import { Lim } from "components/organisms/Lim"
 import { ProgramManager } from "components/organisms/ProgramManager"
+import { Projects } from "components/organisms/Projects"
 import { SendNote } from "components/organisms/SendNote"
 
 export const aboutMe: string = 'About Me'
 export const sendNote: string = 'Send Note'
 export const programManager: string = 'Program Manager'
+export const projects: string = 'Projects'
+export const englicus: string = 'Englicus'
+export const lim: string = 'Lim'
+
 
 export default {
   [aboutMe]: {
     title: aboutMe,
     icon: '/icons/PROGM024.ICO',
     component: (id: string) => <AboutMe key={id} />,
-    readme: '/eyubkh/Portfolio/main/README.md'
+    url: '/eyubkh/Portfolio/main/README.md'
   },
   [sendNote]: {
     title: sendNote,
@@ -22,5 +29,22 @@ export default {
     title: programManager,
     icon: '/icons/PROGM008.ICO',
     component: (id: string) => <ProgramManager key={id} />
+  },
+  [projects]: {
+    title: projects,
+    icon: '/icons/PROGM008.ICO',
+    component: (id: string) => <Projects key={id} />
+  },
+  [englicus]: {
+    title: englicus,
+    icon: '/icons/COMMD001.ICO',
+    url: '/eyubkh/Englicus/main/README.md',
+    component: (id: string) => <Englicus key={id} />
+  },
+  [lim]: {
+    title: lim,
+    icon: '/icons/COMMD001.ICO',
+    url: '/eyubkh/Lim/main/README.md',
+    component: (id: string) => <Lim key={id} />
   }
 }
