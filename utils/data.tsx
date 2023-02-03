53bdf6956282a38cb8c4f817edaab209e7a5204f
eyubkh <eyub.kh@gmail.com>
2023-02-03 13:42:59 +0100

    changing structure

--- utils/data.tsx
+++ utils/data.tsx
@@ -1,14 +1,26 @@
 import { AboutMe } from "components/organisms/AboutMe"
+import { ProgramManager } from "components/organisms/ProgramManager"
 import { SendNote } from "components/organisms/SendNote"
 
-export const aboutMe = {
-  title: 'About Me',
-  icon: '/icons/PROGM024.ICO',
-  component: <AboutMe />
-}
+export const aboutMe: string = 'About Me'
+export const sendNote: string = 'Send Note'
+export const programManager: string = 'Program Manager'
 
-export const sendNote = {
-  title: 'Send Note',
-  icon: '/icons/MSMAI001.ICO',
-  component: <SendNote />
+export default {
+  [aboutMe]: {
+    title: aboutMe,
+    icon: '/icons/PROGM024.ICO',
+    component: (id: string) => <AboutMe key={id} />,
+    readme: '/eyubkh/Portfolio/main/README.md'
+  },
+  [sendNote]: {
+    title: sendNote,
+    icon: '/icons/MSMAI001.ICO',
+    component: (id: string) => <SendNote key={id} />
+  },
+  [programManager]: {
+    title: programManager,
+    icon: '/icons/PROGM008.ICO',
+    component: (id: string) => <ProgramManager key={id} />
+  }
 }
