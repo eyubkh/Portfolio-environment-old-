6c73349fde2a4756d80ca07da073ab00d8087885
eyubkh <eyub.kh@gmail.com>
2023-02-03 13:49:24 +0100

    new init case and removing usesed code

--- utils/windowReducer.ts
+++ utils/windowReducer.ts
@@ -1,33 +1,27 @@
 import { Reducer } from 'react'
-import { WindowStateProps, WindowActionProps, WindowActionOptions } from 'types/lib/windowTypes'
+import {
+  WindowStateProps,
+  WindowActionProps,
+  WindowActionOptions
+} from 'types/lib/windowTypes'
 
 const windowReducer: Reducer<WindowStateProps, WindowActionProps> = (state, action) => {
   const { type, payload } = action
-  const { FULLSCREEN, ICON, TITLE, ID, POSSITION } = WindowActionOptions
+  const { FULLSCREEN, INIT, POSSITION } = WindowActionOptions
   switch (type) {
+    case INIT: {
+      const { id, title } = payload
+      return {
+        ...state,
+        id,
+        title
+      }
+    }
     case FULLSCREEN: {
       return {
         ...state,
         isFullScreen: !state.isFullScreen,
         possition: !state.isFullScreen ? { x: 0, y: 0 } : state.lastPossition
-      }
-    }
-    case ID: {
-      return {
-        ...state,
-        id: '123456' + Math.round(Math.random() * 10)
-      }
-    }
-    case TITLE: {
-      return {
-        ...state,
-        title: payload
-      }
-    }
-    case ICON: {
-      return {
-        ...state,
-        icon: payload
       }
     }
     case POSSITION: {
