83c76707caff6488e81ef474b7ff8a61451e364c
eyubkh <eyub.kh@gmail.com>
2023-02-15 11:25:17 +0000

    adding resizing

--- components/molecules/WindowContent.tsx
+++ components/molecules/WindowContent.tsx
@@ -1,19 +1,31 @@
+import useWindowContext from "@utils/hooks/useWindowContext"
 import { Black100, White100 } from "@utils/tokens"
 import styled from "styled-components"
 import { ChildrenType } from "types/global"
 
-const WindowContentComponent = styled.div`
-  width: 100%;
-  height: 100%;
+const WindowContentComponent = styled.div<any>`
   overflow: scroll;
   background-color: ${White100};
   border: 1px solid ${Black100};
   border-top: none;
+
+  min-width: 200px;
+  min-height: 100px;
+  width: 700px;
+  height: 400px;
+
+  resize: ${({isFullScreen}) => isFullScreen ? 'none': 'both'};
 `
 
 export const WindowContent = ({ children }: ChildrenType): JSX.Element => {
+  const { windowState } = useWindowContext()
+  const { isFullScreen } = windowState
+
   return (
-    <WindowContentComponent>
+    <WindowContentComponent 
+      isFullScreen={isFullScreen}
+      style={{ 'width': isFullScreen ? '100%' : '700px', "height": isFullScreen ? '100%' : '400px' }}
+      >
       {children}
     </WindowContentComponent>
   )
