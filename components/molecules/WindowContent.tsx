15547601a039dd546f4e27384ae4ebd236f48ecb
eyubkh <eyub.kh@gmail.com>
2023-02-21 17:27:20 +0000

    removing logic of resizing

--- components/molecules/WindowContent.tsx
+++ components/molecules/WindowContent.tsx
@@ -1,31 +1,20 @@
-import useWindowContext from "@utils/hooks/useWindowContext"
 import { Black100, White100 } from "@utils/tokens"
 import styled from "styled-components"
 import { ChildrenType } from "types/global"
 
 const WindowContentComponent = styled.div<any>`
-  overflow: scroll;
   background-color: ${White100};
   border: 1px solid ${Black100};
   border-top: none;
+  width: auto;
+  height: 100%;
 
-  min-width: 200px;
-  min-height: 100px;
-  width: 700px;
-  height: 400px;
-
-  resize: ${({isFullScreen}) => isFullScreen ? 'none': 'both'};
+  overflow: scroll;
 `
 
 export const WindowContent = ({ children }: ChildrenType): JSX.Element => {
-  const { windowState } = useWindowContext()
-  const { isFullScreen } = windowState
-
   return (
-    <WindowContentComponent 
-      isFullScreen={isFullScreen}
-      style={{ 'width': isFullScreen ? '100%' : '700px', "height": isFullScreen ? '100%' : '400px' }}
-      >
+    <WindowContentComponent >
       {children}
     </WindowContentComponent>
   )
